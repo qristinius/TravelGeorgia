24c51736bbcfef63ccba8c910b8f46fe424e980c
Ruso kvesitadze <kvesitadzeruso@gmail.com>
2023-03-23 21:13:45 +0400

    update

--- data.py
+++ data.py
@@ -5,56 +5,114 @@ Tours= [
 "location": "Signagi",
 "duration":"2 Days",
 "price":"70",
+"date":"16.04.2023",
+"available_places": "20",
+},
+
+{
+"img":"https://www.got2globe.com/wp-content/uploads/2017/02/kazbegi-igreja-georgia-caucaso.jpg",
+"location": "Kazbegi",
+"duration":"3 Days",
+"price":"100",
+"date":"18.04.2023",
+"available_places": "15",
+},
+
+{
+"img":"https://cdn.georgiantravelguide.com/storage/files/mestia-in-winter-mestiya-zimoy-mestia-ghamit.jpg",
+"location": "Svaneti",
+"duration":"2 Days",
+"price":"80",
+"date":"12.04.2023",
+"available_places": "10",
+},
+
+
+{
+"img":"https://storage.googleapis.com/bd-ge-01/buildings-v2/2560x1920/17136.jpg",
+"location": "Bakuriani",
+"duration":"1 Days",
+"price":"100",
+"date":"18.04.2023",
+"available_places": "25",
+},
+
+
+{
+"img":"https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/Borjomi_bu_Shioshvili.jpg/280px-Borjomi_bu_Shioshvili.jpg",
+"location": "Borjomi",
+"duration":"2 Days",
+"price":"70",
 "date":"14.04.2023",
+"available_places": "20",
+},
+
+{
+"img":"https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Batumi_sunset_2.jpg/280px-Batumi_sunset_2.jpg",
+"location": "Batumi",
+"duration":"3 Days",
+"price":"150",
+"date":"20.04.2023",
+"available_places": "25",
+},
+
+{
+"img": "https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/Sighnagi_2009.jpg/280px-Sighnagi_2009.jpg",
+"location": "Signagi",
+"duration":"3 Days",
+"price":"120",
+"date":"16.04.2023",
 "available_places": "20",
 },
 
 {
 "img":"https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/Sighnagi_2009.jpg/280px-Sighnagi_2009.jpg",
 "location": "Kazbegi",
-"duration":"3 Days",
-"price":"100",
+"duration":"2 Days",
+"price":"80",
 "date":"18.04.2023",
 "available_places": "15",
 },
 
 {
 "img":"https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/Sighnagi_2009.jpg/280px-Sighnagi_2009.jpg",
-"location": "Kazbegi",
+"location": "Svaneti",
 "duration":"3 Days",
-"price":"100",
-"date":"18.04.2023",
-"available_places": "15",
+"price":"150",
+"date":"12.04.2023",
+"available_places": "10",
 },
 
 
 {
 "img":"https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/Sighnagi_2009.jpg/280px-Sighnagi_2009.jpg",
-"location": "Kazbegi",
-"duration":"3 Days",
-"price":"100",
+"location": "Bakuriani",
+"duration":"2 Days",
+"price":"120",
 "date":"18.04.2023",
-"available_places": "15",
+"available_places": "25",
 },
 
 
 {
 "img":"https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/Sighnagi_2009.jpg/280px-Sighnagi_2009.jpg",
-"location": "Kazbegi",
+"location": "Borjomi",
 "duration":"3 Days",
-"price":"100",
-"date":"18.04.2023",
-"available_places": "15",
+"price":"130",
+"date":"14.04.2023",
+"available_places": "20",
 },
 
 {
 "img":"https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/Sighnagi_2009.jpg/280px-Sighnagi_2009.jpg",
-"location": "Kazbegi",
-"duration":"3 Days",
-"price":"100",
-"date":"18.04.2023",
-"available_places": "15",
+"location": "Batumi",
+"duration":"2 Days",
+"price":"10",
+"date":"20.04.2023",
+"available_places": "25",
 },
+
+
 
 ]
 
