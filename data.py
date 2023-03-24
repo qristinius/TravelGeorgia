34ae90f208462e2bdbb531f4efc5fa58019af5e3
Ruso kvesitadze <kvesitadzeruso@gmail.com>
2023-03-24 18:58:53 +0400

    aa

--- data.py
+++ data.py
@@ -56,6 +56,18 @@ Tours= [
 "available_places": "25",
 },
 
+
+
+{
+"img":"https://www.got2globe.com/wp-content/uploads/2017/02/kazbegi-igreja-georgia-caucaso.jpg",
+"location": "Kazbegi",
+"duration":"2 Days",
+"price":"80",
+"date":"18.04.2023",
+"available_places": "15",
+},
+
+
 {
 "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/Sighnagi_2009.jpg/280px-Sighnagi_2009.jpg",
 "location": "Signagi",
@@ -65,37 +77,30 @@ Tours= [
 "available_places": "20",
 },
 
+
 {
-"img":"https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/Sighnagi_2009.jpg/280px-Sighnagi_2009.jpg",
-"location": "Kazbegi",
+"img":"https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Batumi_sunset_2.jpg/280px-Batumi_sunset_2.jpg",
+"location": "Batumi",
 "duration":"2 Days",
-"price":"80",
-"date":"18.04.2023",
-"available_places": "15",
-},
-
-{
-"img":"https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/Sighnagi_2009.jpg/280px-Sighnagi_2009.jpg",
-"location": "Svaneti",
-"duration":"3 Days",
-"price":"150",
-"date":"12.04.2023",
-"available_places": "10",
-},
-
-
-{
-"img":"https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/Sighnagi_2009.jpg/280px-Sighnagi_2009.jpg",
-"location": "Bakuriani",
-"duration":"2 Days",
-"price":"120",
-"date":"18.04.2023",
+"price":"10",
+"date":"20.04.2023",
 "available_places": "25",
 },
 
 
 {
-"img":"https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/Sighnagi_2009.jpg/280px-Sighnagi_2009.jpg",
+"img":"https://cdn.georgiantravelguide.com/storage/files/mestia-in-winter-mestiya-zimoy-mestia-ghamit.jpg",
+"location": "Svaneti",
+"duration":"3 Days",
+"price":"150",
+"date":"17.04.2023",
+"available_places": "15",
+},
+
+
+
+{
+"img":"https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/Borjomi_bu_Shioshvili.jpg/280px-Borjomi_bu_Shioshvili.jpg",
 "location": "Borjomi",
 "duration":"3 Days",
 "price":"130",
@@ -104,12 +109,12 @@ Tours= [
 },
 
 {
-"img":"https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/Sighnagi_2009.jpg/280px-Sighnagi_2009.jpg",
-"location": "Batumi",
-"duration":"2 Days",
-"price":"10",
+"img":"https://www.got2globe.com/wp-content/uploads/2017/02/kazbegi-igreja-georgia-caucaso.jpg",
+"location": "Kazbegi",
+"duration":"3 Days",
+"price":"160",
 "date":"20.04.2023",
-"available_places": "25",
+"available_places": "15",
 },
 
 
